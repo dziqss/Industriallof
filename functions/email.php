357f2419317cd95f2c9fc4415863283817f3b8b7
Slawomir Radzikowski <dzikusss@o2.pl>
2021-03-25 18:50:03 +0100

    formularz kontaktowy, proba walidacji email

--- functions/email.php
+++ functions/email.php
@@ -1,10 +1,35 @@
 <?php
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+   //$email = strtolower($email);
+    $tel = $_POST['tel'];
+    $message = $_POST['message'];
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$tel = $_POST['tel'];
-$message = $_POST['message'];
 
-echo $name.$email.$tel.$message;
+
+// $checkTekst = preg_match('@^[0-9]{2}-[0-9]{3}$@',$tel);
+// echo $tel . "<br>" . $checkTekst;
+
+echo $email . "<br>"; //do sprawdzania lovercase
+
+//walidacja, wyrazenia regularne
+//login a-zA-Z0-9
+//@
+//serwer a-z0-9
+//domena a-z0-9
+$checkEmail = preg_match('*^[a-zA-Z0-9][a-zA-Z0-9\.-_]+@[a-zA-Z0-9]\.[a-z0-9]{2,5}$*', $_POST['email']);
+echo $checkEmail . "<br>";
+
+
+    
+    if ($checkEmail) {
+        echo "E-mail prawidłowy";
+    } else {
+        echo "Niepoprawny adres e-mail";
+
+    }
+
+
 
 ?>
